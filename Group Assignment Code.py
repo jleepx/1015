348e66665e3347b5b63fd01d0cc56aa5cccda894
JaceLeonswift <107034413+JaceLeonswift@users.noreply.github.com>
2023-03-20 13:40:23 +0800

    Update and rename Group Assignment Code to Group Assignment Code.py

---
+++ Group Assignment Code.py
@@ -0,0 +1,16 @@
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import recall_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import plot_tree
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+import numpy as np
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt 
+sb.set()
+np.set_printoptions(suppress=True)
